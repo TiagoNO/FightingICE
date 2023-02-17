ff0f8d6ed484ea3e9d5ca56656e5e62bfcb3c6a6
Tiago <tiagonegri237@gmail.com>
2023-02-17 16:59:57 -0300

    Check existing log data before running the match

--- tournament.py
+++ tournament.py
@@ -1,8 +1,15 @@
 import sys
 import os
 #!/usr/bin/env python3
-from subprocess import DEVNULL, STDOUT, check_call
+import subprocess
+from subprocess import DEVNULL, STDOUT
 
+def check_existing(p1_name, p2_name):
+    files = os.listdir("log/point/")
+    for f in files:
+        if "{}_{}".format(p1_name, p2_name) in f or "{}_{}".format(p2_name, p1_name) in f:
+            return True
+    return False
 
 ai_dir = "data/ai/"
 
@@ -13,6 +20,11 @@ for f in os.listdir(ai_dir):
 
 for i in range(len(ais)):
     for j in range(i+1, len(ais)):
-        print("{} vs {}!".format(ais[i], ais[j]))
-        command = ["java", "-cp", "FightingICE.jar:lib/lwjgl/*:lib/natives/linux/*:lib/*:data/ai/*", "Main", "--a1", "{}".format(ais[i]), "--a2", "{}".format(ais[j]), "--fastmode", "--grey-bg", "--inverted-player", "1", "--limithp", "400", "400", "--n", "2", "--c1", "ZEN", "--c2", "ZEN", "--disable-window"]
-        check_call(command, stdout=DEVNULL, stderr=STDOUT)
+        if check_existing(ais[i], ais[j]):
+            print("{} vs {} already exist...".format(ais[i], ais[j]))
+            continue
+        else:
+            print("{} vs {}!".format(ais[i], ais[j]))
+            command = ["java", "-cp", "FightingICE.jar:lib/lwjgl/*:lib/natives/linux/*:lib/*:data/ai/*", "Main", "--a1", "{}".format(ais[i]), "--a2", "{}".format(ais[j]), "--fastmode", "--grey-bg", "--inverted-player", "1", "--limithp", "400", "400", "--n", "2", "--c1", "ZEN", "--c2", "ZEN", "--disable-window"]
+            process = subprocess.Popen(command, stdout=DEVNULL, stderr=DEVNULL)
+            process.wait()
